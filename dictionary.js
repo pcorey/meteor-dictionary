c88bcb63afaf2ba476602161712bf9fc1ab29b2c
Pete Corey <petecorey@gmail.com>
2014-12-17 09:04:25 -0800

    Added dictionary publish

--- dictionary.js
+++ dictionary.js
@@ -1,6 +1,18 @@
 Dictionary = new Meteor.Collection('dictionary');
 
 if (Meteor.isServer) {
+    Meteor.publish('dictionary', function(regex, options, sort, limit) {
+        regex = regex || null;
+        sort = sort   || {word: 1};
+        limit = limit || 10;
+        return Dictionary.find({
+            word: {$regex: regex, $options: options}
+        }, {
+            limit: limit,
+            sort: sort
+        });
+    });
+
     Meteor.startup(function() {
         if (Dictionary.find().count() > 0) {
             return;
--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-  name: 'pcorey:dictionary',
-  summary: 'Creates Dictionary collection populated with Webster\'s Unabridged English Dictionary.',
-  version: '1.0.0',
-  git: 'https://github.com/pcorey/meteor-dictionary.git'
+    name: 'pcorey:dictionary',
+    summary: 'Creates Dictionary collection populated with Webster\'s Unabridged English Dictionary.',
+    version: '1.0.0',
+    git: 'https://github.com/pcorey/meteor-dictionary.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.1');
-  api.addFiles('dictionary.js');
-  api.addFiles('dictionary.json', 'server', {isAsset: true});
+    api.versionsFrom('1.0.1');
+    api.addFiles('dictionary.js');
+    api.addFiles('dictionary.json', 'server', {isAsset: true});
 });
